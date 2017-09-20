0f25d66d60d008f3109a0d7c50f16460a4843200
Cars45 Tech <cars45tech@Cars45s-MacBook-Air.local>
2017-09-20 11:12:45 +0100

    Version 1.0.9

--- react-device-frame.js
+++ react-device-frame.js
@@ -1,6 +1,75 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var _createClass=function(){function defineProperties(target,props){for(var i=0;i<props.length;i++){var descriptor=props[i];descriptor.enumerable=descriptor.enumerable||!1;descriptor.configurable=!0;if("value" in descriptor)descriptor.writable=!0;Object.defineProperty(target,descriptor.key,descriptor)}}return function(Constructor,protoProps,staticProps){if(protoProps)defineProperties(Constructor.prototype,protoProps);if(staticProps)defineProperties(Constructor,staticProps);return Constructor}}();var _react=require("react");var _react2=_interopRequireDefault(_react);var _propTypes=require("prop-types");var _propTypes2=_interopRequireDefault(_propTypes);require("./lib/css/devices.min.css");function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj}}
-function _classCallCheck(instance,Constructor){if(!(instance instanceof Constructor)){throw new TypeError("Cannot call a class as a function")}}
-function _possibleConstructorReturn(self,call){if(!self){throw new ReferenceError("this hasn't been initialised - super() hasn't been called")}return call&&(typeof call==="object"||typeof call==="function")?call:self}
-function _inherits(subClass,superClass){if(typeof superClass!=="function"&&superClass!==null){throw new TypeError("Super expression must either be null or a function, not "+typeof superClass)}subClass.prototype=Object.create(superClass&&superClass.prototype,{constructor:{value:subClass,enumerable:!1,writable:!0,configurable:!0}});if(superClass)Object.setPrototypeOf?Object.setPrototypeOf(subClass,superClass):subClass.__proto__=superClass}
-var Device=function(_Component){_inherits(Device,_Component);function Device(){_classCallCheck(this,Device);return _possibleConstructorReturn(this,(Device.__proto__||Object.getPrototypeOf(Device)).apply(this,arguments))}
-_createClass(Device,[{key:"render",value:function render(){var classes="device-"+this.props.device+" device-"+this.props.color;return _react2.default.createElement("div",{className:classes},_react2.default.createElement("div",{className:"device-frame"},_react2.default.createElement("div",{className:"device-content"},_react2.default.createElement("iframe",{className:"device-src",src:this.props.url}))),_react2.default.createElement("div",{className:"device-stripe"}),_react2.default.createElement("div",{className:"device-header"},_react2.default.createElement("div",{className:"device-sensors"})),_react2.default.createElement("div",{className:"device-btns"}),_react2.default.createElement("div",{className:"device-power"}))}}]);return Device}(_react.Component);Device.propTypes={device:_propTypes2.default.string,color:_propTypes2.default.string,url:_propTypes2.default.string};exports.default=Device
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+var _propTypes = require("prop-types");
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
+require("./lib/css/devices.min.css");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Device = function (_Component) {
+  _inherits(Device, _Component);
+
+  function Device() {
+    _classCallCheck(this, Device);
+
+    return _possibleConstructorReturn(this, (Device.__proto__ || Object.getPrototypeOf(Device)).apply(this, arguments));
+  }
+
+  _createClass(Device, [{
+    key: "render",
+    value: function render() {
+
+      var classes = "device-" + this.props.name + " device-" + this.props.color;
+
+      return _react2.default.createElement(
+        "div",
+        { className: classes },
+        _react2.default.createElement(
+          "div",
+          { className: "device-frame" },
+          _react2.default.createElement(
+            "div",
+            { className: "device-content" },
+            _react2.default.createElement("iframe", { className: "device-src", src: this.props.url })
+          )
+        ),
+        _react2.default.createElement("div", { className: "device-stripe" }),
+        _react2.default.createElement(
+          "div",
+          { className: "device-header" },
+          _react2.default.createElement("div", { className: "device-sensors" })
+        ),
+        _react2.default.createElement("div", { className: "device-btns" }),
+        _react2.default.createElement("div", { className: "device-power" })
+      );
+    }
+  }]);
+
+  return Device;
+}(_react.Component);
+
+Device.propTypes = {
+  name: _propTypes2.default.string,
+  color: _propTypes2.default.string,
+  url: _propTypes2.default.string
+};
+
+exports.default = Device;
